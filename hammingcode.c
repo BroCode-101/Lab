95777f224d42e6321bc36ae5d8bf48be75a53c07
Saran <103126696+BroCode-101@users.noreply.github.com>
2025-03-27 11:44:31 +0530

    Update hammingcode.c

--- hammingcode.c
+++ hammingcode.c
@@ -1,48 +1,16 @@
-#include <stdio.h>
-#include <math.h>
+) Create the following tables with the mapping given below.
 
-void calculateParityBits(int data[], int n) {
-    int r = 0, m = n;
-    while ((m + r + 1) > pow(2, r)) {
-        r++;
-    }
-    int hammingCode[m + r + 1];
-    int j = 0, k = 0;
-    for (int i = 1; i <= m + r; i++) {
-        if ((i & (i - 1)) == 0) {
-            hammingCode[i] = 0;
-        } else {
-            hammingCode[i] = data[j++];
-        }
-    }
-    for (int i = 1; i <= m + r; i++) {
-        if ((i & (i - 1)) == 0) {
-            int parity = 0;
-            for (int k = i; k <= m + r; k++) {
-                if ((k & i) == i) {
-                    parity ^= hammingCode[k];
-                }
-            }
-            hammingCode[i] = parity;
-        }
-    }
-    printf("Encoded Hamming Code: ");
-    for (int i = 1; i <= m + r; i++) {
-        printf("%d", hammingCode[i]);
-    }
-    printf("\n");
-}
+Employee (Empno, Ename, Job, MgrId, DoB, DoJ, Sal, Comm, Deptno)
 
-int main() {
-    int n;
-    printf("Enter the number of data bits: ");
-    scanf("%d", &n);
-    int data[n];
-    
-    for (int i = 0; i < n; i++) {
-        printf("Enter the data bits: ");
-        scanf("%d", &data[i]);
-    }
-    calculateParityBits(data, n);
-    return 0;
-}
+Department (Dname, Deptno, Dloc)
+
+(a) Display the Emp no, name, salary and experience of each employee ordered by salary (highest to lowest)
+
+(b) List the names of the employee working for “Marketing” Department.
+
+(c) List the names of the employees born in the current month.
+
+
+
+ii) Write a PL/SQL function to display the details of the employee when Employee no given as input. Handle Exceptions.
+
